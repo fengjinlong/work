98b5f7143f42c2fcde79c2021c95f8e2278d04fc
fengjinlong <18618166564.@163.com>
2017-12-01 17:13:28 +0800

    first

---
+++ 新版移动端/js/category.js
@@ -0,0 +1,100 @@
+$(function () {
+
+//获取初始高度，考虑微信,暂时不调用
+  function initscrollHeight () {
+    var wechat = 130;
+    var footer = $('.fix').height();
+    var header = $('header').height();
+    var screen = $(window).height();
+
+    var initheight = (screen - header - footer - wechat) / 75 + 'rem';
+    // $('.scrollOne').css({height: initheight,overflow: 'hidden'})
+    // scrollOne.refresh()
+  }
+// 初始化
+  var scrollOne = new BScroll('.scrollOne',{
+    scrollY: true,
+    tap: true,
+    probeType: 3
+  })
+  var scrollTwo = new BScroll('.scrollTwo',{
+    scrollY: true,
+    click: true,
+    probeType: 3
+  })
+
+  // 点击左侧
+  $('.scrollOne li').on('tap', function () {
+    var index = $('.scrollOne li').index($(this))
+    var dom = '.list' + index
+    if(!$(this).hasClass('active')){
+      $('.scrollOne li.active').removeClass('active')
+      $(this).addClass('active')
+    }
+    scrollToEle(dom)
+  })
+
+  // 右侧被滚动
+  function scrollToEle (dom) {
+    scrollTwo.scrollToElement(dom, 0)
+  }
+
+  // 获取右侧数组
+  var ARR = getHeightArr ()
+  function getHeightArr () {
+    var arr = []
+    var val = 0
+    $('.lists .list').each(function () {
+      val += $(this).height()
+      arr.push(val)
+    })
+    return arr
+  }
+
+  // 右侧开始滑动
+  rightSlide()
+  function rightSlide () {
+    var touch = {}
+    $('.contwo').on('touchstart',function (e) {
+      touch.page1X = e.touches[0].pageX
+      touch.page1Y = e.touches[0].pageY
+    })
+    scrollTwo.on('scroll',function (pos) {
+      touch.page2X = pos.x
+      touch.page2Y = pos.y
+      var x = Math.abs(touch.page2X-touch.page1X);
+      var y = Math.abs(touch.page2Y-touch.page1Y);
+      if(x > y){
+          return
+      }else{
+          returnIndex(pos.y)
+      }
+    })
+  }
+
+  // 返回index
+  function returnIndex (n) {
+     n = Math.abs(n)
+    for (var i=0; i<ARR.length-1; i++) {
+        if(n <= ARR[0]) {
+            leftToEle(0)
+            return
+        }
+        if(n > ARR[i] && n <= ARR[i+1]){
+            leftToEle(i+1)
+            return
+        }
+        if(n >= ARR[ARR.length]){
+            leftToEle(ARR.length)
+            return
+        }
+    }
+  }
+  // 左侧被滚动
+  function leftToEle (index) {
+    var dom = '.li' + index
+    scrollOne.scrollToElement(dom, 50)
+    $('.scrollOne li.active').removeClass('active')
+    $('.scrollOne li'+dom).addClass('active')
+  }
+})
---
+++ 新版移动端/js/discount.js
@@ -0,0 +1,4 @@
+$(function () {
+  // 延迟加载
+  $("img.lazy").lazyload({effect: "fadeIn"});
+})
