173f037e3eafd9203c509d6eb07c9914610b5c8b
fengjinlong <18618166564.@163.com>
2017-12-14 15:24:37 +0800

    ad

---
+++ 新版移动端/js/categoryCom.js
@@ -0,0 +1,10 @@
+  var scrollOne = new BScroll('.scrollOne',{
+    scrollY: true,
+    click: true,
+    probeType: 3
+  })
+  var scrollTwo = new BScroll('.scrollTwo',{
+    scrollY: true,
+    click: true,
+    probeType: 3
+  })
